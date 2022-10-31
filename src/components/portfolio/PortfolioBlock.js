ac53e928097b30d24d9f2733146c120ac6bc7a3c
Melbine <melbineyt@gmail.com>
2022-10-31 12:47:57 +0300

    first commit

---
+++ src/components/portfolio/PortfolioBlock.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import IconLink from './IconLink';
+import { Box } from '@mui/material';
+
+function PortfolioBlock(props) {
+  const { image, live, source, title } = props;
+  return (
+    <Box
+      display={'flex'}
+      flexDirection={'column'}
+      justifyContent={'center'}
+      alignItems={'center'}
+      marginTop={'100px'}>
+      <Box component={'img'} src={image} alt={'mockup'} width={'50%'} />
+      <h1 style={{ fontSize: '2rem' }}>{title}</h1>
+      <Box
+        className={'portfolio'}
+        display={'flex'}
+        flexDirection={'column'}
+        gap={'0.5rem'}
+        alignItems={'center'}
+        fontSize={'1.5rem'}
+        py={'2rem'}>
+        <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
+          <IconLink link={live} title={'Live Demo'} icon={'fa fa-safari'} />
+        </Box>
+        <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
+          <IconLink link={source} title={'Source Code'} icon={'fa fa-code'} />
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default PortfolioBlock;
